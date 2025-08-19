4fa1150e18942226475428c5d843bbd44ad5dd45
thiagofrancasantos <francasantos.t@gmail.com>
2025-08-19 15:51:06 -0400

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './features/1layout/layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: LayoutComponent
+    },
+    {
+        path: 'layout',
+        component: LayoutComponent
+    }
+];
