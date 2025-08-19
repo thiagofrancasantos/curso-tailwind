061c38ce07d1a242d642f260fa36cbc5e4b07956
Thiago Franca <francasantos.t@gmail.com>
2025-08-19 21:57:19 +0000

    flex-box done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,18 @@
 import { Routes } from '@angular/router';
 import { LayoutComponent } from './features/1layout/layout/layout.component';
+import { FlexboxComponent } from './features/2flexbox/flexbox/flexbox.component';
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: LayoutComponent
-    },
-    {
-        path: 'layout',
-        component: LayoutComponent
-    }
+  {
+    path: '',
+    component: FlexboxComponent,
+  },
+  {
+    path: 'layout',
+    component: LayoutComponent,
+  },
+  {
+    path: 'flexbox',
+    component: FlexboxComponent,
+  },
 ];
---
+++ src/app/features/2flexbox/flexbox/flexbox.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-flexbox',
+  standalone: true,
+  imports: [],
+  templateUrl: './flexbox.component.html',
+  styleUrl: './flexbox.component.css'
+})
+export class FlexboxComponent {
+
+}
